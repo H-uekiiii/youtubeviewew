7703a8047808033cc6ec11b4934dd07c979fc987
unknown <hiroki_bomber@icloud.com>
2020-09-12 01:34:07 +0900

    お気に入りボタンのトップページへの適用

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
 import { addDecorator } from '@storybook/react';
-import GlobalStyle from '../src/style/GlobalStyle';
+
+import { action } from '@storybook/addon-actions';
+import { FavoriteProvider } from '../src/contexts/FavoriteContext';
+
+import GlobalStyle from "../src/style/GlobalStyle";
 
 addDecorator(storyFn => (
   <MemoryRouter
@@ -9,6 +13,21 @@ addDecorator(storyFn => (
   >
     {storyFn()}
   </MemoryRouter>
+));
+
+
+const mockApi = {
+  get: async () => {
+    // モック実装
+    action('api.get')();
+    return { data: [] };
+  },
+};
+
+addDecorator((storyFn) => (
+  <FavoriteProvider api={mockApi}>
+    {storyFn()}
+  </FavoriteProvider>
 ));
 
 addDecorator(storyFn => <><GlobalStyle />{storyFn()}</>);
